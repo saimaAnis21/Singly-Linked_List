a287b5bbb14829025bc350008af44bd2ecf73229
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-10 17:31:00 +0500

    Initial commit

--- challenge.rb
+++ challenge.rb
@@ -9,13 +9,35 @@ end
 
 class LinkedList
   #setup head and tail
+  attr_accessor :head, :tail
+
+  def initialize
+    @head = Node.new(nil)
+    @tail = Node.new(nil)
+  end
 
   def add(number)
     # your code here
+    if @head.next_node.nil?
+      new_node = Node.new(number)
+      @head.next_node= new_node
+      @tail=new_node
+    else
+      prev_node=@tail
+      new_node = Node.new(number)
+      @tail = new_node
+      prev_node.next_node = @tail
+    end
   end
 
   def get(index)
     # your code here
+    node=@head.next_node
+    
+    index.times do
+       node = node.next_node
+     end
+     node.value
   end
 end
 
@@ -23,5 +45,5 @@ list = LinkedList.new
 
 list.add(3)
 list.add(5)
-puts list.get(1)
+puts list.get(0)
 # => 5
